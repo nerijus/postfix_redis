5d5817882273eb3b017836248ea8f50e57fc8819
Nerijus Baliūnas <nerijus@users.sourceforge.net>
2018-09-12 23:53:21 +0300

    add basic tests

--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -5,14 +5,23 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 
-unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
-  end
+describe package('postfix') do
+  it { should be_installed }
+end
+describe package('redis') do
+  it { should be_installed }
 end
 
-# This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+describe service('postfix') do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe service('redis') do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe user('mailuser') do
+  it { should exist }
 end

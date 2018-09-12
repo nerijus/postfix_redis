563bd1eb086eaf5e460817365d661585d3e8306c
Nerijus Baliūnas <nerijus@users.sourceforge.net>
2018-09-12 20:57:21 +0300

    move scripts to /opt/postfix_redis

--- recipes/postfix-user.rb
+++ recipes/postfix-user.rb
@@ -2,31 +2,25 @@ user 'mailuser' do
   name 'mailuser'
   home '/home/mailuser'
   manage_home true
-  notifies :create, 'template[/home/mailuser/store.py]'
-  notifies :create, 'template[/home/mailuser/get.py]'
   action :create
 end
 
-template '/home/mailuser/store.py' do
+directory '/opt/postfix_redis' do
+  mode 0755
+end
+
+template '/opt/postfix_redis/store.py' do
   source 'store.py.erb'
-  user 'mailuser'
-  group 'mailuser'
   mode 0755
   notifies :run, 'execute[chcon-postfix]'
 end
 
-template '/home/mailuser/get.py' do
+template '/opt/postfix_redis/get.py' do
   source 'get.py.erb'
-  user 'mailuser'
-  group 'mailuser'
   mode 0755
 end
 
 execute 'chcon-postfix' do
-  command 'chcon -t postfix_local_exec_t /home/mailuser/store.py'
+  command 'chcon -t postfix_local_exec_t /opt/postfix_redis/store.py'
   action :nothing
-end
-
-directory '/home/mailuser' do
-  mode 0755
 end

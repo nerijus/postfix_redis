607686c73139385c587b698bd0f177400ba7b6b0
Nerijus Baliūnas <nerijus@users.sourceforge.net>
2018-09-14 10:37:43 +0300

    added 2 more tests and comment

--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -8,6 +8,7 @@
 describe package('postfix') do
   it { should be_installed }
 end
+
 describe package('redis') do
   it { should be_installed }
 end
@@ -26,6 +27,15 @@ describe user('mailuser') do
   it { should exist }
 end
 
+describe file("/opt/postfix_redis/get.py") do
+  it { should exist }
+end
+
+describe file("/opt/postfix_redis/store.py") do
+  it { should exist }
+end
+
+# send test email to tcp port 25 and check if redis returns expected output
 describe command("printf 'mail from:<test@test.lt>\\nrcpt to:<test@vinted>\\ndata\\n123\\n.\\n' > /dev/tcp/127.1/25 && sleep 4 && /opt/postfix_redis/get.py test@vinted") do
   its("stdout") { should match "From.*\nReturn.*\nX-Original-To: test@vinted\nDelivered-To:.*\nReceived:.*\n.*by.*\n.*for.*\nMessage-Id:.*\nDate:.*\nFrom:.*\n^$\n123\n" }
 end
